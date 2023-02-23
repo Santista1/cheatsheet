5adbf95a82ae75ffad1248e5b316901c6fa6348b
Santista1 <santista1@proton.me>
2023-02-23 19:32:38 -0300

    v1

---
+++ data.js
@@ -0,0 +1,125 @@
+const data =
+
+[
+
+  [
+
+    [
+
+      [
+        [ <p> Html </p>, <p> English </p> ],
+        [ <p> Basic </p> ],
+        [ <b> Code </b>, <b> Appearance </b> ],
+        [ "<p> Text </p>", <p> Text </p> ],
+        [ "<a> Link </a>", <a href="#"> Link </a> ],
+        [ "<button> Button </button>", <button className="button"> Button </button> ],
+        [ "<img src='kiwi.jpg'>", <img src="kiwi.jpg"/> ],
+        [ "<input type='text'>", <input type="text"/> ],
+      ],
+
+      [
+        [ <p> Html </p>, <p> English </p>],
+        [ <p> Text </p>],
+        [ <b> Code </b>, <b> Appearance </b> ],
+        [ "<p> Paragraph </p>", <p> Paragraph </p> ],
+        [ "<span> Inline </span>", <span> Inline </span> ],
+        [ "<b> Bold </b>", <b> Bold </b> ],
+        [ "<i> Italic </i>", <i> Italic </i> ],
+        [ "<del> Deleted </del>", <del> Deleted </del> ],
+        [ "<mark> Marked </mark>", <mark> Marked </mark> ],
+        [ "<h6> Title </h6>", <h6> Title </h6> ],
+        [ "<h5> Title </h5>", <h5> Title </h5> ],
+        [ "<h4> Title </h4>", <h4> Title </h4> ],
+        [ "<h3> Title </h3>", <h3> Title </h3> ],
+        [ "<h2> Title </h2>", <h2> Title </h2> ],
+        [ "<h1> Title </h1>", <h1> Title </h1> ],
+        [ "<strong> Important </strong>", <strong> Important </strong> ],
+      ],
+
+      [
+        [ <p> Html </p>, <p> English </p> ],
+        [ <p> Listas </p> ],
+        [ <b> Code </b>, <b> Appearance </b> ],
+      ],
+
+    ],
+
+    [
+
+      [
+        [ <p> Css </p>, <p> English </p> ],
+         [<p> Basic </p> ],
+        [ <b> Code </b>, <b> Appearance </b> ],
+      ],
+
+      [
+        [ <p> Css </p>, <p> English </p> ],
+        [ <p> Text </p> ],
+        [ <b> Code </b>, <b> Appearance </b> ],
+      ],
+
+    ],
+
+  ],
+
+  [
+
+    [
+
+      [
+        [ <p> Html </p>, <p> Español </p> ],
+        [ <p> Basico </p> ],
+        [ <b> Codigo </b>, <b> Aparencia </b> ],
+        [ "<p> Texto </p>", <p> Texto </p> ],
+        [ "<a> Enlace </a>", <a href="#"> Enlace </a> ],
+        [ "<button> Boton </button>",<button className="button"> Boton </button> ],
+        [ "<img src='kiwi.jpg'>", <img src="kiwi.jpg"/> ],
+        [ "<input type='text'>", <input type="text"/> ],
+      ],
+
+      [
+        [ <p> Html </p>, <p> Español </p> ],
+        [ <p> Texto </p> ],
+        [ <b> Codigo </b>, <b> Aparencia </b> ],
+        [ "<p> Parrafo </p>", <p> Parrafo </p> ],
+        [ "<span> En línea </span>", <span> En línea </span> ],
+        [ "<b> Negrita </b>", <b> Negrita </b> ],
+        [ "<i> Italico </i>", <i> Italico </i> ],
+        [ "<del> Tachado </del>", <del> Tachado </del> ],
+        [ "<mark> Marcado </mark>", <mark> Marcado </mark> ],
+        [ "<h6> Titulo </h6>", <h6> Titulo </h6> ],
+        [ "<h5> Titulo </h5>", <h5> Titulo </h5> ],
+        [ "<h4> Titulo </h4>", <h4> Titulo </h4> ],
+        [ "<h3> Titulo </h3>", <h3> Titulo </h3> ],
+        [ "<h2> Titulo </h2>", <h2> Titulo </h2> ],
+        [ "<h1> Titulo </h1>", <h1> Titulo </h1> ],
+        [ "<strong> Importante </strong>", <strong> Importante </strong> ],
+      ],
+
+      [
+        [ <p> Html </p>, <p> Español </p> ],
+        [ <p> Listas </p> ],
+        [ <b> Codigo </b>, <b> Aparencia </b> ],
+      ],
+
+    ],
+
+    [
+
+      [
+        [ <p> Css </p>, <p> Español </p> ],
+        [ <p> Basico </p> ],
+        [ <b> Codigo </b>, <b> Aparencia </b> ],
+      ],
+
+      [
+        [ <p> Css </p>, <p> Español </p> ],
+        [ <p> Texto </p> ],
+        [ <b> Codigo </b>, <b> Aparencia </b> ],
+      ],
+
+    ],
+
+  ],
+
+]
